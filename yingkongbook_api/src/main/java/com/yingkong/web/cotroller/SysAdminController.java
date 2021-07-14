13d4f67c783119d5615663c23de57803f5a8e0c5
ykongchenwei <2373079918@qq.com>
2021-07-14 17:22:21 +0800

    555

--- yingkongbook_api/src/main/java/com/yingkong/web/cotroller/SysAdminController.java
+++ yingkongbook_api/src/main/java/com/yingkong/web/cotroller/SysAdminController.java
@@ -40,7 +40,7 @@ public class SysAdminController {
         if(id!=null){
            SysAdmin admin =  sysAdminService.getAdminById(id);
            if(admin!=null){
-               System.out.println("123");
+               System.out.println("18883");
                return BasicResponse.success(admin);
            }else {
                System.out.println("nihao");
